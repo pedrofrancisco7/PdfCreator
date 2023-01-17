f874aa25ccdf49abbc790d0a621b5e96d61a5da7
Pedro Rodrigues <40839750+pedrofrancisco7@users.noreply.github.com>
2023-01-17 18:17:14 -0300

    pdf creator

---
+++ PdfGenerator/Controllers/PdfCreatorController.cs
@@ -0,0 +1,81 @@
+﻿using DinkToPdf;
+using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using PdfGenerator.Utility;
+
+namespace PdfGenerator.Controllers
+{
+    [Route("api/pdfcreator")]
+    [ApiController]
+    public class PdfCreatorController : Controller
+    {
+        private IConverter _converter;
+        public PdfCreatorController(IConverter converter)
+        {
+            _converter = converter;
+        }
+        [HttpGet("CreatePDF")]
+        public IActionResult CreatePDF()
+        {
+            var globalSettings = new GlobalSettings
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings { Top = 10 },
+                DocumentTitle = "PDF Report",
+                Out = @"D:\PDFCreator\Employee_Report.pdf"
+            };
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                HtmlContent = TemplateGenerator.GetHTMLString(),
+                WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "assets", "styles.css") },
+                HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
+                FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
+            };
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings }
+            };
+            
+            _converter.Convert(pdf);
+            
+
+
+            return Ok("Successfully created PDF document.");
+        }
+
+        [HttpGet("GetPDF")]
+        public IActionResult GetPDF()
+        {
+            var globalSettings = new GlobalSettings
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings { Top = 10 },
+                DocumentTitle = "PDF Report"
+            };
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                HtmlContent = TemplateGenerator.GetHTMLString(),
+                WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "assets", "styles.css") },
+                HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
+                FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
+            };
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings }
+            };
+            var file = _converter.Convert(pdf);
+            //return File(file, "application/pdf");
+
+            return File(file, "application/pdf", "EmployeeReport.pdf");
+
+        }
+    }
+}
